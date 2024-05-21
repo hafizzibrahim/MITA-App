cd93a83b0cede4de1429699a10b3ffa739116d9c
Hafiz <hafizzibrahimm0@gmail.com>
2024-05-21 11:10:48 +0700

    update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,29 +14,63 @@ class HomePage extends StatelessWidget {
               children: [
                 ClipRRect(
                   borderRadius: BorderRadius.circular(50),
-                  child: Image.asset('assets/icons/ic_logo.png', scale: 18,),
+                  child: Image.asset(
+                    'assets/icons/ic_logo.png',
+                    scale: 18,
+                  ),
                 ),
-                SizedBox(width: 16,),
+                SizedBox(
+                  width: 16,
+                ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Halo, Hafiz Ibrahim', style: blackTextStyle.copyWith(fontSize: 14, fontWeight: bold),),
-                    Text('Selamat Datang di MITA !', style: greyTextStyle.copyWith(fontSize: 14, fontWeight: bold),),
+                    Text(
+                      'Halo, Hafiz Ibrahim',
+                      style: blackTextStyle.copyWith(
+                          fontSize: 14, fontWeight: bold),
+                    ),
+                    Text(
+                      'Selamat Datang di MITA !',
+                      style: greyTextStyle.copyWith(
+                          fontSize: 14, fontWeight: bold),
+                    ),
                   ],
                 ),
               ],
             ),
           ),
-          const SizedBox(height: 16,),
+          const SizedBox(
+            height: 16,
+          ),
           infoCarousel(context),
-          const SizedBox(height: 16,),
+          const SizedBox(
+            height: 16,
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Artikel Seputar ASD', style: blackTextStyle.copyWith(fontSize: 14, fontWeight: bold),),
-                const SizedBox(height: 16,),
+                containerTest(context),
+                SizedBox(
+                  height: 16,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                    child: buttonTest()
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Artikel Seputar ASD',
+                  style:
+                      blackTextStyle.copyWith(fontSize: 14, fontWeight: bold),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
                 infoContainer(context),
                 infoContainer(context),
                 infoContainer(context),
@@ -54,25 +88,34 @@ class HomePage extends StatelessWidget {
   Widget infoCarousel(BuildContext context) {
     return Container(
       child: CarouselSlider(
-          items: [1, 2, 3, 4, 5].map((i){
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.symmetric(horizontal: 15),
-              decoration: BoxDecoration(
-                color: orangeColor,
-                borderRadius: BorderRadius.circular(10),
+        items: [
+          "assets/images/img_banner1.png",
+          "assets/images/img_banner2.png",
+          "assets/images/img_banner3.png",
+        ].map((String imagePath) {
+          return Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
               ),
-              child: Center(child: Text("Text $i", style: blackTextStyle.copyWith(fontWeight: bold, fontSize: 14),)),
-            );
-          }).toList(),
-          options: CarouselOptions(
+            ),
+          );
+        }).toList(),
+        options: CarouselOptions(
           height: 120,
-            
-      )),
+        ),
+      ),
     );
   }
 
-  Widget infoContainer(BuildContext context){
+  Widget infoContainer(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
       child: Container(
@@ -85,19 +128,124 @@ class HomePage extends StatelessWidget {
         child: Row(
           children: [
             ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-                child: Image.asset('assets/icons/ic_logo.png', scale: 20,)
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/icons/ic_logo.png',
+                  scale: 20,
+                )),
+            SizedBox(
+              width: 16,
             ),
-            SizedBox(width: 16,),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Judul', style: blackTextStyle.copyWith(fontWeight: extraBold, fontSize: 14),),
-                Text('Deskripsi', style: blackTextStyle.copyWith(fontWeight: medium, fontSize: 10),)
+                Text(
+                  'Judul',
+                  style: blackTextStyle.copyWith(
+                      fontWeight: extraBold, fontSize: 14),
+                ),
+                Text(
+                  'Deskripsi',
+                  style:
+                      blackTextStyle.copyWith(fontWeight: medium, fontSize: 10),
+                )
               ],
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget containerTest(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: orangeColor, borderRadius: BorderRadius.circular(10)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: whiteColor, borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Hasil Test',
+                  style: blackTextStyle.copyWith(
+                      fontWeight: semiBold, fontSize: 14),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  '-',
+                  style:
+                      blackTextStyle.copyWith(fontSize: 24, fontWeight: bold),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Terapi A',
+                style:
+                    whiteTextStyle.copyWith(fontWeight: semiBold, fontSize: 14),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hari ke',
+                    style: whiteTextStyle.copyWith(
+                        fontWeight: semiBold, fontSize: 14),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    '1',
+                    style: whiteTextStyle.copyWith(
+                        fontWeight: semiBold, fontSize: 32),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    '/ 30 day',
+                    style: whiteTextStyle.copyWith(
+                        fontWeight: semiBold, fontSize: 14),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buttonTest() {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: orangeColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        'Mulai Test',
+        style: whiteTextStyle.copyWith(fontWeight: bold, fontSize: 14),
       ),
     );
   }
